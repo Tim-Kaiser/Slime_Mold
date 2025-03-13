653d1fe9f84afbd2f8dc11281cb872032d8030b1
tk <60766886+Tim-Kaiser@users.noreply.github.com>
2025-03-14 00:55:29 +0100

    Initial commit

---
+++ OpenGL-Boilerplate/Main.cpp
@@ -0,0 +1,61 @@
+#define GLFW_INCLUDE_NONE
+
+#include "Window.h"
+#include "ShaderLoader.h"
+#include "ObjectLoader.h"
+#include "Model.h"
+
+#include <vector>
+#include <glm.hpp>
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
+int main(int argc, char* arfv[]) {
+
+	Window window("OpenGL Boilerplate");
+	window.setKeycallback(key_callback);
+
+
+
+	//===== SHADER INIT =====
+	ShaderLoader shaderLoader;
+
+	std::unique_ptr<Shader> renderShader = shaderLoader.CreateShaders();
+	shaderLoader.CompileShaders("Shaders/main.vert", renderShader->m_vertexShaderID);
+	shaderLoader.CompileShaders("Shaders/main.frag", renderShader->m_fragmentShaderID);
+
+	shaderLoader.AttachShaders(*renderShader);
+	shaderLoader.LinkProgram(*renderShader);
+
+	Object obj;
+	loadObject("Objects/quad.obj", obj);
+	Model quadModel(&obj, false);
+
+	double startTime = glfwGetTime();
+
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	while (window.Open()) {
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		GLfloat timeSinceStart = (float(glfwGetTime()) - float(startTime));
+		shaderLoader.SendUniformData("time", timeSinceStart);
+
+		quadModel.Render();
+
+		window.Update();
+	}
+
+	shaderLoader.DetachShaders(*renderShader);
+
+	shaderLoader.DestroyShaders(*renderShader);
+	shaderLoader.DestroyProgram(*renderShader);
+
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# OpenGL Boilerplate Template
+
+1. Install [Visual Studio](https://visualstudio.microsoft.com/de/downloads/)
+2. Create a new repository based on this template
+3. Clone the new repository
+4. Open ```OpenGL-Boilerplate.sln``` with Visual Studio
+
+
+---
+---
+This creates a fullscreen window (Exit with ESC) and renders a quad with a basic shader. 
+
+---
+
+# Renaming the project
+
+1. Open the project in Visual Studio and rename it
+2. Close Visual Studio
+3. Rename ALL folders and files that include the original project name (.sln, .vcxproj, etc...)
+4. Open the .sln file in an editor and rename the project path
+5. Open the project in Visual Studio, fix any reference errors
